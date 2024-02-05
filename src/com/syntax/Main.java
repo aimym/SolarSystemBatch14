994a468fa8a9c61182ac153ca74190cd8496341f
aimym <aidananuri25@gmail.com>
2024-02-05 11:00:42 -0500

    The Basic Structure of the Solar systems

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,12 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("The Basic Structure is Ready please write down your code in this "+
+                "project");
+
+    }
+}
